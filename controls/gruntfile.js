94d7b774395e49f424bf7c292eb2ed46438eb6c5
Matt Morgan <m@getplatypi.com>
2014-08-20 16:59:47 -0500

    added gruntfile for combining less

---
+++ controls/gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = exports = function load(grunt) {
+    var config = {
+        bundle: {
+            main: {
+                rootModule: 'platui',
+                license: './license.txt',
+                version: '<%= pkg.version %>',
+                src: './app/index.html',
+                dest: [
+                    './platypusui.ts'
+                ],
+                disableLint: true
+            }
+        },
+        less: {
+            main: {
+                license: './license.txt',
+                version: '<%= pkg.version %>',
+                src: './app/index.html',
+                ignore: [
+                    './controls/less/variables.less'
+                ],
+                dest: [
+                    './platypus.less'
+                ]
+            }
+        },
+        pkg: grunt.file.readJSON('package.json')
+    };
+    
+    grunt.initConfig(config);
+    grunt.loadTasks('../../less-bundle/less-bundle/less-bundle');
+    grunt.loadNpmTasks('grunt-ts-bundle');
+    // By default, run all tests.
+    grunt.registerTask('default', ['bundle', 'less']);
+};
