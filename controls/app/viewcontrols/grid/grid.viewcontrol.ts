1720af2b36cab035e2b9ea591df29407740b0ce0
Darion Welch <darion.welch@gmail.com>
2014-08-28 18:24:04 -0500

    created demo sections on main for all ui elements

--- controls/app/viewcontrols/grid/grid.viewcontrol.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path='../../../controls/lib/platypus.d.ts' />
-module app {
-    export class GridViewControl extends plat.ui.WebViewControl {
-        title = 'Grid';
-        templateUrl = 'viewcontrols/grid/grid.viewcontrol.html';
-        context = {};
-    }
-    plat.register.viewControl('grid', GridViewControl, null, ['grid']);
-}
