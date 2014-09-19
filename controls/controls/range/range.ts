e99a1f8872e67941da720ed2efcc86a84ce10a2e
Matt Morgan <m@getplatypi.com>
2014-09-19 18:45:48 -0500

    added range declaration

---
+++ controls/controls/range/range.ts
@@ -0,0 +1,18 @@
+﻿module platui {
+    /**
+     * @name Range
+     * @memberof platui
+     * @kind class
+     * 
+     * @extends {platui.Slider}
+     * 
+     * @description
+     * An extension of the {@link platui.Slider|Slider} control that allows for a lower and upper value, 
+     * thus creating a variable range of values.
+     */
+    export class Range extends Slider {
+
+    }
+
+    plat.register.control(__Range, Range);
+}
