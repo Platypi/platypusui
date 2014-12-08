c1c509d08490d77cf4473f954c81b9db33316b15
m-s-morgan <m@getplatypi.com>
2014-12-08 15:00:27 -0600

    changed __transitionHash reference to __transitionNegate and fixed drawer swipe mixup. Also cleaned up modal code

--- controls/controls/references/references.ts
+++ controls/controls/references/references.ts
@@ -68,7 +68,7 @@ var __Promise = '$Promise',
      * Misc
      */
     __Flipped = '-flipped',
-    __transitionHash: plat.IObject<string> = {
+    __transitionNegate: plat.IObject<string> = {
         right: 'left',
         left: 'right',
         up: 'down',
