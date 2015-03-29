694b48f4012af8262755a2de91e9a825c124fe70
Will Johnston <will@getplatypi.com>
2015-03-29 12:53:36 -0500

    checking for innerTemplate before accessing it

--- controls/controls/input/input.ts
+++ controls/controls/input/input.ts
@@ -302,6 +302,10 @@ module platui {
                 return;
             }
 
+            if (!_utils.isDocumentFragment(this.innerTemplate)) {
+                return;
+            }
+
             var placeholder = this.innerTemplate.textContent.replace(/\r|\n/g, '');
             if (!_utils.isEmpty(placeholder)) {
                 input.placeholder = placeholder;
