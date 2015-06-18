1cb3f664c34078e6f4518571942e2034ec6b1775
Matt Morgan <m@getplatypi.com>
2015-06-18 16:15:58 -0500

    new build

--- dist/platypusui.js
+++ dist/platypusui.js
@@ -6,7 +6,7 @@ var __extends = this.__extends || function (d, b) {
 };
 /* tslint:disable */
 /**
- * PlatypusUI v0.4.11 (https://platypi.io)
+ * PlatypusUI v0.4.12 (https://platypi.io)
  * Copyright 2015 Platypi, LLC. All rights reserved.
  *
  * PlatypusUI is licensed under the MIT license found at
