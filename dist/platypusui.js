7905ec3e6dfaa7c4645fb7c46d78f2f71231f110
Will Johnston <will@getplatypi.com>
2015-07-23 10:40:31 -0500

    dist

--- dist/platypusui.js
+++ dist/platypusui.js
@@ -6,7 +6,7 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 /* tslint:disable */
 /**
- * PlatypusUI v0.5.1 (https://platypi.io)
+ * PlatypusUI v0.5.2 (https://platypi.io)
  * Copyright 2015 Platypi, LLC. All rights reserved.
  *
  * PlatypusUI is licensed under the MIT license found at
