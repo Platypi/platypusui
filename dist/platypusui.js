17df857a435195a09d31dbf81494162f20db48af
Matt Morgan <m@getplatypi.com>
2015-08-11 18:23:15 -0500

    cleaned up variable names

--- dist/platypusui.js
+++ dist/platypusui.js
@@ -6,7 +6,7 @@ var __extends = this.__extends || function (d, b) {
 };
 /* tslint:disable */
 /**
- * PlatypusUI v0.6.5 (https://platypi.io)
+ * PlatypusUI v0.6.6 (https://platypi.io)
  * Copyright 2015 Platypi, LLC. All rights reserved.
  *
  * PlatypusUI is licensed under the MIT license found at
