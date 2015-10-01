3e1ba1a0444a2f1468647d57bb46fc0930f0ceab
Matt Morgan <m@getplatypi.com>
2015-10-01 11:24:36 -0500

    patched Navbar

--- dist/platypusui.d.ts
+++ dist/platypusui.d.ts
@@ -1,5 +1,5 @@
 /**
-  * PlatypusUI v0.8.2 (https://platypi.io)
+  * PlatypusUI v0.8.3 (https://platypi.io)
   * Copyright 2015 Platypi, LLC. All rights reserved.
   *
   * PlatypusUI is licensed under the MIT license found at
@@ -3465,6 +3465,7 @@ declare module platui {
       * Defines the available bindings for a single component of the Navbar control.
       */
     interface INavbarComponent {
+        [x: string]: any;
         /**
           * The content contained inside the component.
           */
