08b861a392e1453a5a79b2ee3d4eb2adaa3e37ee
m-s-morgan <m@getplatypi.com>
2016-05-19 09:37:23 -0500

    ios 9 animation patch

--- dist/platypusui.js
+++ dist/platypusui.js
@@ -6,7 +6,7 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 /* tslint:disable */
 /**
- * PlatypusUI v0.15.1 (https://platypi.io)
+ * PlatypusUI v0.15.2 (https://platypi.io)
  * Copyright 2015 Platypi, LLC. All rights reserved.
  *
  * PlatypusUI is licensed under the MIT license found at
