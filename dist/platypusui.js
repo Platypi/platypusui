e8ad03a9ebeeae655b3efd10ea3fcc3698d38908
William Johnston <johnston.w@gmail.com>
2018-04-12 18:11:04 -0500

    dist

--- dist/platypusui.js
+++ dist/platypusui.js
@@ -11,7 +11,7 @@ var __extends = (this && this.__extends) || (function () {
 })();
 /* tslint:disable */
 /**
- * PlatypusUI v0.16.6 (https://platypi.io)
+ * PlatypusUI v0.16.7 (https://platypi.io)
  * Copyright 2015 Platypi, LLC. All rights reserved.
  *
  * PlatypusUI is licensed under the MIT license found at
